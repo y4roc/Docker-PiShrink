1fbe2be922202c17d1f0b5706ea8f0a0f6aa2915
thomas <th@sonntagnacht.de>
2018-07-27 12:41:42 +0200

    remove unused code

--- docker.sh
+++ docker.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-##Update Apache UID
-uid=$(id -u)
-if [ $uid -gt 100000 ]; then
-	uid=1000
-fi
-
-sed "s/\$USER_ID/$uid/g" ./pishrink/Dockerfile.dist > ./pishrink/Dockerfile
-
 docker-compose build
 docker-compose up -d
 docker-compose run pi_shrink bash
